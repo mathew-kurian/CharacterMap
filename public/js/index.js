4aa5e9ec78cd419958fec34f15f896016c302b38
Mathew Kurian <bluejamesbond@gmail.com>
2015-01-12 22:42:52 -0600

    Updating package.json

--- public/js/index.js
+++ public/js/index.js
@@ -123,11 +123,11 @@ function renderGlyphItem(canvas, glyphIndex) {
         xmax = (cellWidth + glyphWidth) / 2,
         x0 = xmin;
 
-    ctx.fillStyle = '#e67e22';
-    ctx.fillRect(xmin - cellMarkSize + 2, fontBaseline, cellMarkSize, 2);
-    ctx.fillRect(xmin, fontBaseline, 2, cellMarkSize);
-    ctx.fillRect(xmax, fontBaseline, cellMarkSize, 2);
-    ctx.fillRect(xmax, fontBaseline, 2, cellMarkSize);
+    // ctx.fillStyle = '#e67e22';
+    // ctx.fillRect(xmin - cellMarkSize + 2, fontBaseline, cellMarkSize, 2);
+    // ctx.fillRect(xmin, fontBaseline, 2, cellMarkSize);
+    // ctx.fillRect(xmax, fontBaseline, cellMarkSize, 2);
+    // ctx.fillRect(xmax, fontBaseline, 2, cellMarkSize);
 
     ctx.fillStyle = '#FFFFFF';
 
